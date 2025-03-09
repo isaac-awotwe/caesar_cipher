d3de5528cc98bfb13863aa53ca1f5796faec9c91
Isaac Awotwe <isaacawotwe@gmail.com>
2025-03-08 18:50:29 -0700

    Modify the scrip and add an art.py file with the caesar cipher logo

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,8 +1,10 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+# Import and print logo
+from art import logo
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+print(logo)
+
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 
 # TODO-1: Create a function called 'encrypt()' that takes 'original_text' and 'shift_amount' as 2 inputs.
@@ -14,35 +16,59 @@ shift = int(input("Type the shift number:\n"))
 
 # TODO-3: Call the 'encrypt()' function and pass in the user inputs. You should be able to test the code and encrypt a
 #  message.
-alpha_length = len(alphabet)
 
-def encrypt(original_text, shift_amount):
-    encrypted_text = ""
+
+# TODO-1: Create a function called 'decrypt()' that takes 'original_text' and 'shift_amount' as inputs.
+# TODO-2: Inside the 'decrypt()' function, shift each letter of the 'original_text' *backwards* in the alphabet
+#  by the shift amount and print the decrypted text.
+# TODO-3: Combine the 'encrypt()' and 'decrypt()' functions into one function called 'caesar()'.
+#  Use the value of the user chosen 'direction' variable to determine which functionality to use.
+
+# def encrypt(original_text, shift_amount):
+#     cipher_text = ""
+#     for letter in original_text:
+#         shifted_position = (alphabet.index(letter) + shift_amount) % len(alphabet)
+#         cipher_text += alphabet[shifted_position]
+#     print(f"Here is the encoded result: {cipher_text}")
+
+
+#encrypt(original_text=text, shift_amount=shift)
+
+
+# def decrypt(original_text, shift_amount):
+#     cipher_text = ""
+#     for letter in original_text:
+#         original_position = (alphabet.index(letter) - shift_amount) % len(alphabet)
+#         cipher_text += alphabet[original_position]
+#     print(f"Here is the original text: {cipher_text}")
+
+
+def caesar_cipher(original_text, shift_amount, encode_or_decode):
+    if encode_or_decode == "decode":
+        shift_amount*=-1
+    cipher_text = ""
     for letter in original_text:
         if letter not in alphabet:
-            encrypted_text+=letter
+            cipher_text+=letter
         else:
-            new_position = alphabet.index(letter) + shift_amount
-            if new_position > alpha_length:
-                encrypted_text += alphabet[(new_position % alpha_length) - 1]
-            else:
-                encrypted_text += alphabet[new_position]
-    print(f"Here is the encoded result: {encrypted_text}")
+            shifted_position = (alphabet.index(letter) + shift_amount) % len(alphabet)
+            cipher_text += alphabet[shifted_position]
+    print(f"Here is the {encode_or_decode}d result: {cipher_text}")
 
 
-def decrypt(original_text, shift_amount):
-    decrypted_text = ""
-    for letter in original_text:
-        if letter not in alphabet:
-            decrypted_text+=letter
-        else:
-            decrypted_text += alphabet[alphabet.index(letter) - shift_amount]
-    print(f"Here is the decoded result: {decrypted_text}")
-            
-def caesar(way, original_text, shift_number):
-    if way == "encode":
-        encrypt(original_text, shift_number)
-    else:
-        decrypt(original_text, shift_number)
+run_again = True
 
-caesar(direction,text, shift)
+
+while run_again:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    caesar_cipher(original_text = text, shift_amount = shift, encode_or_decode = direction)
+
+    run_again_question = input("Type 'yes' if you want to go again. Otherwise type 'no.'\n").lower()
+
+    if run_again_question == "no":
+        print("Goodbye")
+        run_again = False
+
